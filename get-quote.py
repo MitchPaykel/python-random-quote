d50d28f0a220188f95d88f71e879866702606df4
RixAmner <m.paykel@gmail.com>
2020-04-19 18:08:13 -0500

    Random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -7,9 +7,12 @@ def prime():
   quotes = f.readlines()
   f.close()
 
-  last = 13
+  last = len(quotes) - 1
+  rnd = random.randint(0, last)  
+  print(quotes[rnd])
   rnd = random.randint(0, last)
   print(quotes[rnd])
+
 
 if __name__== "__main__":
   prime()
